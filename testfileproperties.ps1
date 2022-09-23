f1c6662543d7521e35ddd1977a7fd535d0760ed6
David Thomson <davidthomson123@hotmail.com>
2022-09-23 16:41:42 +0100

    File Dimensions for images in folder

---
+++ testfileproperties.ps1
@@ -0,0 +1,25 @@
+﻿Add-Type -AssemblyName System.Drawing
+
+$ImageList = Get-ChildItem "C:\Temp\images"
+
+$LandscapePath = "C:\Temp\Landscape"
+$PortraitPath = "C:\Temp\Portrait"
+
+foreach ($ImageFile in $ImageList)
+{
+    #Get the image information
+    $image = New-Object System.Drawing.Bitmap $ImageFile.Fullname
+    #Get the image attributes
+    $ImageHeight = $image.Height
+    $ImageWidth = $image.Width
+    #Close the image
+    $image.Dispose()
+    If($ImageWidth -gt $ImageHeight)
+    {
+        Move-Item -LiteralPath $ImageFile.FullName -Destination $LandscapePath -Force
+    }
+    Else
+    {
+        Move-Item -LiteralPath $ImageFile.FullName -Destination $PortraitPath -Force
+    }
+}
