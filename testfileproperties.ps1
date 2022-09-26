6c64a943bd3a03362359cb54374dbb84adb48ace
David Thomson <davidthomson123@hotmail.com>
2022-09-26 20:33:49 +0100

    Add recurse check for images

--- testfileproperties.ps1
+++ testfileproperties.ps1
@@ -1,25 +1,29 @@
 ﻿Add-Type -AssemblyName System.Drawing
 
-$ImageList = Get-ChildItem "C:\Temp\images"
+$ImageList = Get-ChildItem -Path "C:\Temp\images" -Filter *.png -Recurse | ForEach-Object { [System.Drawing.Image]::FromFile($_.FullName) } | Where-Object { $_.Width -gt 400 -and $_.Height -gt 400 }
 
-$LandscapePath = "C:\Temp\Landscape"
-$PortraitPath = "C:\Temp\Portrait"
+# $ImageList = Get-ChildItem -Path "C:\Temp\images" -Filter CopyForbuild.bat -Recurse -ErrorAction SilentlyContinue -Force
 
-foreach ($ImageFile in $ImageList)
+# $ImageList = Get-ChildItem "C:\Temp\images"
+
+# $LandscapePath = "C:\Temp\Landscape"
+# $PortraitPath = "C:\Temp\Portrait"
+
+foreach ($image in $ImageList)
 {
     #Get the image information
-    $image = New-Object System.Drawing.Bitmap $ImageFile.Fullname
+    # $image = New-Object System.Drawing.Bitmap $ImageFile.Fullname
     #Get the image attributes
     $ImageHeight = $image.Height
     $ImageWidth = $image.Width
     #Close the image
     $image.Dispose()
-    If($ImageWidth -gt $ImageHeight)
-    {
-        Move-Item -LiteralPath $ImageFile.FullName -Destination $LandscapePath -Force
-    }
-    Else
-    {
-        Move-Item -LiteralPath $ImageFile.FullName -Destination $PortraitPath -Force
-    }
+    # If($ImageWidth -gt $ImageHeight)
+    # {
+    #     Move-Item -LiteralPath $ImageFile.FullName -Destination $LandscapePath -Force
+    # }
+    # Else
+    # {
+    #     Move-Item -LiteralPath $ImageFile.FullName -Destination $PortraitPath -Force
+    # }
 }
