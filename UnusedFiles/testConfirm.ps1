af5403dd2c6afaadd1f9336148f82ea556a8ed12
David Thomson <davidthomson123@hotmail.com>
2022-10-03 16:13:02 +0100

    include testing script for passing whatif and confirm onwards

---
+++ UnusedFiles/testConfirm.ps1
@@ -0,0 +1,34 @@
+﻿function ShouldTestCallee {
+    [cmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+    param($test)
+    Begin {        
+    }
+    Process {
+        if ($PSCmdlet.ShouldProcess($env:COMPUTERNAME, "Confirm?")) {
+            Write-Host "test"
+        }
+    }
+    end {}
+} 
+
+function ShouldTestCaller {     
+    [cmdletBinding(SupportsShouldProcess=$true)]     
+    param($test,$list) 
+    
+    try 
+    {
+        foreach($number in $list){
+            ShouldTestCallee 
+    } 
+    }
+    catch 
+    {
+        Throw "$($_.Exception.Message)"
+    } 
+    
+}  
+    
+$list = 0..3
+$ConfirmPreference = 'High' 
+ShouldTestCaller -list $list -Confirm
+# ShouldTestCaller -Confirm
