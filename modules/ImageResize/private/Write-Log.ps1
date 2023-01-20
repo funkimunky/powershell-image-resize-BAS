6f89a29ef85dba347b9228ce549791cc7f9e367e
David Thomson <davidthomson123@hotmail.com>
2023-01-20 10:05:08 +0000

    Refactoriing not working though

---
+++ modules/ImageResize/private/Write-Log.ps1
@@ -0,0 +1,22 @@
+﻿Function Write-Log{
+    <#
+    .SYNOPSIS
+        Writes string to log file
+    .DESCRIPTION
+        Writes string to log file       
+    .EXAMPLE
+        Write-Log("log content") -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>    
+    
+    [cmdletbinding()]
+    param(
+        [string] $Text
+    )
+    $fileDate = Get-Date -Format FileDate
+    $fileDateTime = Get-Date -Format ddMMyy-HH:mm
+    $exportPath = "C:\temp\process{0}.txt" -f $fileDate
+    $logText = "{0}`t{1}" -f $fileDateTime, $Text
+
+    $logText | Tee-Object -FilePath $exportPath -Append
+}
